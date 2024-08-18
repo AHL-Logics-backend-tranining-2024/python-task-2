f2d5fb96be384ad1cad27f12fb579463d47104b3
[Mohhammad Ballal] <[mohammadoballal@gmail.com]>
2024-08-18 18:47:45 +0300

    complete utilities module by adding four main Function for validate date, status and priority and get valid input from the user

---
+++ MohammadBallal/utils.py
@@ -0,0 +1,26 @@
+#Function for validate the format of date
+def validate_date(datestr):
+    from datetime import datetime
+    try: 
+        #Validates if the date matches dd/mm/yyyy format be strptime() method 
+        datetime.strptime(datestr, '%d/%m/%Y')
+    except ValueError: 
+        raise ValueError("Date form must be dd/mm/yyyy")
+
+#Function to get valid details input by user    
+def get_valid_input(prompt):
+    userInput= input(prompt)
+    if not userInput:
+        raise ValueError("It's empty!")
+
+#Function for validate status if its in the valid status list or not
+def validate_status(status):
+    validStatus= ['InProgress','Completed']
+    if status not in validate_status:
+        raise ValueError(f"Ststus should be one of : {validate_status}")
+
+#Function for validate priority if its in the valid priority list or not
+def valid_priority(priority):
+    validPriority= ['High', 'Medium', 'Low']
+    if priority not in valid_priority:
+        raise ValueError(f"Priority should be one of : {valid_priority}")
