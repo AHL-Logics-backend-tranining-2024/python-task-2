b52ff53bfa43c04e6c5bb2f39d84781bb07bb700
Hatem29 <hatm.h.s2011@hotmail.com>
2024-08-31 13:00:28 +0300

    utils.py module finished

--- HatemHashlamon/utils.py
+++ HatemHashlamon/utils.py
@@ -1,11 +1,22 @@
-def validate_date(date_str):
-    pass
+import datetime
 
-def get_valid_input(prompt):
-    pass
+def validate_date(date_str: str):
+    try:
+        # tries to parse the string to a datetime, if not possible thorugh exception
+        datetime.strptime(date_str, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError(f"Date '{date_str}' is not in the correct format (YYYY-MM-DD).")
+    
+def get_valid_input(prompt: str):
+    user_input = input(prompt)  
+    if user_input == "": 
+        raise ValueError("The input is empty")
+    return user_input
 
 def validate_status(status):
-    pass
+    if status != "InProgress" and status != "Completed":
+        raise ValueError(f"the status '{status}' provided is not recognized.")
 
 def validate_priority(priority): 
-    pass
+    if priority not in ('High', 'Medium', 'Low'):
+        raise ValueError(f"the priority '{priority}' provided is not recognized.")
