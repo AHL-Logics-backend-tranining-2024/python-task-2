6927e594561716a4ef73060aa4dc94bd2f34db33
Hatem29 <hatm.h.s2011@hotmail.com>
2024-08-31 12:44:06 +0300

    define the major modules, and created task.py classes with thier implementations

---
+++ HatemHashlamon/task.py
@@ -0,0 +1,36 @@
+class Task:
+    def __init__(self, task_id, title, description, due_date, status):
+        self.task_id = task_id
+        self.title = title
+        self.description = description
+        self.due_date = due_date
+        self.status = status
+    
+    def update_details(self, title=None, description=None, due_date=None, status=None):
+        if title is not None:
+            self.title = title
+        if description is not None:
+            self.description = description
+        if due_date is not None:
+            self.due_date = due_date
+        if status is not None:
+            self.status = status
+
+    def display(self):
+        return (f"Task ID: {self.task_id}\n"
+            f"Title: {self.title}\n"
+            f"Description: {self.description}\n"
+            f"Due Date: {self.due_date}\n"
+            f"Status: {self.status}")
+
+
+class UrgentTast(Task):
+    def __init__(self, task_id, title, description, due_date, status, priority):
+        self.priority = priority
+        super().__init__(task_id, title, description, due_date, status)
+    
+    def display(self):
+        return super.display() + f"\nPriority: {self.priority}"
+
+    
+        
---
+++ HatemHashlamon/utils.py
@@ -0,0 +1,11 @@
+def validate_date(date_str):
+    pass
+
+def get_valid_input(prompt):
+    pass
+
+def validate_status(status):
+    pass
+
+def validate_priority(priority): 
+    pass
